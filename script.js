5d2095d3f85f5177e51b1a511ef6151ef0637394
virdz <35266539+virdz@users.noreply.github.com>
2021-03-30 00:27:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var amount = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    //var d = Date();
+    //var y = d.getFullYear();
+    //var finish = y + years;
+    var interest = (amount * rate/100)*years;
+    document.getElementById("result").innerHTML = "If you deposit " + amount + ",\n" + "at an interest rate of "+ rate + "%.\n" + "You will recieve an ammount of " + interest + ",\nin the year "; // + finish;
 }
-        
+function updateRate(){
+    document.getElementById("rateValue").innerHTML = document.getElementById("rate").value+"%";
+}
