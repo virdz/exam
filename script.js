4e00b4a3084550fdc32fbc0b5996108926d794cc
virdz <35266539+virdz@users.noreply.github.com>
2021-03-30 00:46:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function compute(){
         //var y = d.getFullYear();
         var finish = 2021 + years;
         var interest = (amount * rate/100)*years;
-        document.getElementById("result").innerHTML = "If you deposit " + amount + ",\n" + "at an interest rate of "+ rate + "%.\n" + "You will recieve an ammount of " + interest + ",\nin the year " + finish;
+        document.getElementById("result").innerHTML = "If you deposit " + amount + ",\n" + "at an interest rate of "+ rate + "%.\n" + "You will recieve an amount of " + interest + ",\nin the year " + finish;
     } else {
         alert("the ammount must be > 0");
     }
