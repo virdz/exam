96dee60294b1d6fc0fcfd70498c4d6f3e1db42ce
virdz <35266539+virdz@users.noreply.github.com>
2021-03-30 00:35:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-function compute() {
-    var amount = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    //var d = Date();
-    //var y = d.getFullYear();
-    //var finish = y + years;
-    var interest = (amount * rate/100)*years;
-    document.getElementById("result").innerHTML = "If you deposit " + amount + ",\n" + "at an interest rate of "+ rate + "%.\n" + "You will recieve an ammount of " + interest + ",\nin the year "; // + finish;
+function compute(){
+    if(document.getElementById("principal").value > 0 ) {
+        var amount = document.getElementById("principal").value;
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        //var d = Date();
+        //var y = d.getFullYear();
+        //var finish = y + years;
+        var interest = (amount * rate/100)*years;
+        document.getElementById("result").innerHTML = "If you deposit " + amount + ",\n" + "at an interest rate of "+ rate + "%.\n" + "You will recieve an ammount of " + interest + ",\nin the year "; // + finish;
+    } else {
+        alert("the ammount must be > 0");
+    }
 }
 function updateRate(){
     document.getElementById("rateValue").innerHTML = document.getElementById("rate").value+"%";
